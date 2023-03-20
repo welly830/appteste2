de31379becc36bfde2544af425926b69eee8babd
welly830 <welly830@gmail.com>
2023-03-20 18:01:48 -0300

    svelte2

---
+++ src/lib/Teste.svelte
@@ -0,0 +1,13 @@
+<script>
+   export let label = 'Teste';
+
+</script>
+
+
+<h1 class="btn" >{label}</h1>
+
+<style>
+    .btn {
+        color: #ab0;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import BottomNav from "$lib/BottomNav.svelte";
+  import Header from "$lib/Header.svelte";
+  import Logo from "$lib/Logo.svelte";
+  
+
+  
+import "../app.postcss";
+</script>
+
+
+
+<Header/>
+
+<BottomNav/>
+
+
+<slot />
